507404b209003b104f2a676464ae4e08c724d7cd
Chris Baugh <chris.baugh@gmail.com>
2021-10-20 10:59:12 -0500

    Update to work under more conditions

--- R/setwd_to_file.R
+++ R/setwd_to_file.R
@@ -1,13 +1,21 @@
 #' @title setwd_to_file
 #'
 #' @description Sets the current working directory to the file location
-#'
+#'              This only works for interactive and files being run directly,
 #' @examples
 #' setwd_to_file
 #' @export
 
 setwd_to_file <- function() {
-  #if (!interactive())
-  # Add in functionality for non Rstudio
-  setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+  if (interactive()) {
+    os <- .Platform$OS.type
+    file.directory <- switch(os,
+                             'unix'    = system('pwd', intern = TRUE),                          # R
+                             'windows' = dirname(rstudioapi::getActiveDocumentContext()$path))  # Rstudio
+  } else {
+    cArgs <- commandArgs(trailingOnly = FALSE)
+    file.directory <- normalizePath(sub("--file=", "", cArgs[grep("--file=", cArgs)]))                                               # Rscript
+  }
+  setwd(file.directory)
 }
+

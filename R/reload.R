75c221123e6618b0b708011aeaa3854d846077a0
Phioria <chris.baugh@gmail.com>
2025-01-25 11:52:30 -0500

    Update config storage directory to be CRAN acceptable

--- R/reload.R
+++ R/reload.R
@@ -1,10 +1,10 @@
 #' @title reload
 #'
 #' @description Clears the environment and reloads the current package
-#' 
+#'
 #' @param clear_env Set to FALSE if you don't want the environment cleared. Defaults to TRUE
 #' @param use_rcpp Set to FALSE if you're not using Rcpp. Defaults to TRUE
-#'              
+#'
 #' @examples
 #' reload()
 #' reload(clear_env = FALSE)
@@ -17,54 +17,50 @@ reload <- function(clear_env = TRUE) {
     message("The package 'devtools' is missing.")
     message("Please install it before continuing.")
   }
-  
+
   # Clear the environment
   if (clear_env) {
     clr()
   }
-  
-  home <- get_home()
-  if (is.null(home)) {
-    # A message will print from get_home() if this is the case
-    return(NULL)
-  }
-  
-  settings_file <- file.path(home, ".reloader")
+
+  cfg <- tools::R_user_dir(package = "phior", which = "config")
+
+  settings_file <- file.path(cfg, ".reloadr")
   if (!file.exists(settings_file)) {
     message("Could not locate the reload settings file.")
     message("Run setup_reload() to create this file.")
     return(NULL)
   }
-  
+
   settings <- read.delim(settings_file, header = FALSE)
-  
+
   if (ncol(settings) != 2 && nrow(settings) != 1) {
     message("The reload settings file is malformed.")
     message("Run setup_reload() to overwrite the existing file.")
     return(NULL)
   }
-  
+
   colnames(settings) <- c("package_dir", "working_dir")
-  
+
   if (!dir.exists(settings$package_dir)) {
     message(paste("The package directory:", settings$package_dir, "doesn't exist."))
     return(NULL)
   }
-  
+
   if (!dir.exists(settings$working_dir)) {
     message(paste("The working directory:", settings$working_dir, "doesn't exist."))
     return(NULL)
   }
-  
+
   # Now that all the guardrails have been passed
   setwd(settings$package_dir)
-  
+
   if ('Rcpp' %in% installed.packages()) {
     Rcpp::compileAttributes(verbose = getOption("verbose"))
   }
   devtools::document()
   #devtools::build_readme()
   devtools::load_all()
-  
+
   setwd(settings$working_dir)
 }
--- R/setup_reload.R
+++ R/setup_reload.R
@@ -4,22 +4,22 @@
 #'
 #' @param package_dir A string representing the base directory of the R package in development
 #' @param working_dir A string representing the directory you are working in
-#' 
+#'
 #' @examples
-#' setup_reload(package_dir = "C:/packages/my_package", working_dir = "C:/Analysis/analysis_23") 
+#' setup_reload(package_dir = "C:/packages/my_package", working_dir = "C:/Analysis/analysis_23")
 #' setup_reload(package_dir = file.path(get_home(), "Documents", "Packages", "my_package"), working_dir = file.path(get_desktop(), "my_working_directory"))
 #' @export
 
 setup_reload <- function(package_dir, working_dir) {
-  home <- get_home()
-  if (is.null(home)) {
-    # A message will print from get_home() if this is the case
-    return(NULL)
+  # Get CRAN acceptable persistant storage location for config file
+  cfg <- tools::R_user_dir(package = "phior", which = "config")
+
+  if (!file.exists(cfg)) {
+    stopifnot(dir.create(cfg))
   }
-  
-  settings_file <- file.path(home, ".reloader")
+
+  settings_file <- file.path(cfg, ".reloadr")
   settings <- data.frame(package_dir, working_dir)
-  
+
   write.table(settings, settings_file, sep = "\t", col.names = FALSE, row.names = FALSE)
-  
 }
